4522678b2ae868f1bb5f0a1dbf194640ae05f782
ErickDumas <91906484+ErickDum@users.noreply.github.com>
2025-05-25 22:30:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,9 +15,10 @@ echo "=============================================================="
 echo "============== INSTALACION DE LIBRERIAS NS3 =================="
 echo "=============================================================="
 sudo apt-get install libc6-dev -y
-apt-get install sqlite sqlite3 libsqlite3-dev -y 
+sudo apt-get install sqlite sqlite3 libsqlite3-dev -y 
 sudo apt-get install sqlite3 libsqlite3-dev -y
 sudo apt-get install libeigen3-dev -y
+sudo apt-get install gcc g++ python3 python3-pip cmake -y
 
 # Install ns-3 5G LENA
 echo "=============================================================="
@@ -30,7 +31,38 @@ cd contrib
 git clone https://gitlab.com/cttc-lena/nr.git
 cd nr
 git checkout tags/v2x-1.1 -b v2x-1.1-branch
-cd ../..
+cd ..
+
+echo "=============================================================="
+echo "================= INSTALACION DE NS3-GYM ====================="
+echo "=============================================================="
+sudo apt-get install libzmq5 libzmq5-dev -y
+sudo apt-get install libprotobuf-dev -y
+sudo apt-get install protobuf-compiler -y
+cd contrib
+git clone https://github.com/tkn-tub/ns3-gym
+mv ./ns3-gym/ ./opengym 
+cd ..
+sed -i '207d' ./contrib/opengym/examples/linear-mesh-2/sim.cc
+sed -i '352d' ./contrib/opengym/examples/linear-mesh/sim.cc
+
+
+echo "=============================================================="
+echo "=================== COMPILACION DE NS3 ======================="
+echo "=============================================================="
+
 ./ns3 configure --enable-tests --enable-examples
 ./ns3 build
 
+echo "=============================================================="
+echo "================= INSTALL PYTHON OPENGYM ====================="
+echo "=============================================================="
+python3 -m venv venv
+source venv/bin/activate
+cd ./contrib/opengym/ 
+pip install -U ./model/ns3gym
+cd ../..
+
+echo "=============================================================="
+echo "================== INSTALACION COMPLETADA ===================="
+echo "=============================================================="
