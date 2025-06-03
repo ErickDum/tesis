bcb6071cfd1c21ef42ae8e4f716fc7627cdd1c8f
Andres2800 <92129319+Andres2800@users.noreply.github.com>
2025-06-03 10:04:41 -0500

    runRL

--- rl_training/runRL.sh
+++ rl_training/runRL.sh
@@ -14,12 +14,45 @@ generate_triangular() {
     }'
 }
 
+# Parametros de simulacion
+interferentes_maximos=33
+paso_interferentes=2
+tamano_platoon=4
+init_time=40
+sim_time=60
+path="/home/tesis/Documentos/simulaciones/"
+scenario="urban"
+
 # Número total de simulaciones
 total_runs=10000
 
 # Ejecutar simulaciones
 for ((i = 1; i <= total_runs; i++)); do
+
+
+    # Generación de trips aleatorios
+    python3 /opt/sumo/tools/randomTrips.py \
+        -v -b 0 -e 500 -p 1 \
+        -n ./mapas/${scenario}/map_${scenario}.net.xml \
+        -r ./mapas/${scenario}/map_${scenario}.rou.xml
+
+    # Limpiar fichero temporal
+    rm -f trips.trips.xml
+
+    # Crear rutas de pelotón e interferentes
+    python3 ./mapas/${scenario}/make_${scenario}_map.py "$i"
+
+    # Ejecutar SUMO
+    sumo -c ./mapas/${scenario}/map_${scenario}.sumocfg --fcd-output ./mapas/${scenario}/trace_${scenario}.xml
+
+    # Exportar trace a formato NS2/Mobility
+    python3 /opt/sumo/tools/traceExporter.py \
+        -i ./mapas/${scenario}/trace_${scenario}.xml \
+        --ns2mobility-output=./scratch/mobility.tcl
+
+
     int_num=$(generate_triangular)
-    echo "Run $i with IntNum=$int_num"
-    ./ns3 run "scratch/v2x.cc --resourceAllocationMethod=4 --IntNum=$int_num"
+    echo "Episodio $i con $int_num interferentes"
+    #./ns3 run "scratch/v2x.cc --resourceAllocationMethod=4 --IntNum=$int_num"
+    ./ns3 run "scratch/v2x.cc --slBearerActivationTime=$init_time --simTime=$sim_time --ueNum=$tamano_platoon --IntNum=$int_num  --outputDir=$path --sensingMethod=1 --resourceAllocationMethod=4" 
 done
