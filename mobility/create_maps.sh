d0b3df4a099b83cb654fa7e4f502d73bcfdf96a9
Erick <erick.dumas@ucuenca.edu.ec>
2025-06-18 09:59:37 -0500

    add mobility

--- mobility/create_maps.sh
+++ mobility/create_maps.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
-max_interferentes=33
-step=1
-sims_per_step=10
+max_interferentes=31
 scenario="urban"  # Change this to the desired scenario (e.g., "urban", "highway".)
 script_path="./sumo/$scenario/"
+total=3000
 
-for ((i=1; i<max_interferentes; i+=step))
+generate_triangular() {
+    awk -v a=1 -v b=30 -v c=15 'BEGIN {
+        u = rand();
+        Fc = (c - a) / (b - a);
+        if (u < Fc) {
+            x = a + sqrt(u * (b - a) * (c - a));
+        } else {
+            x = b - sqrt((1 - u) * (b - a) * (b - c));
+        }
+        printf "%d\n", int(x + 0.5);  # Redondear al entero más cercano
+    }'
+}
+
+for ((i = 1; i <=total; i++))
 do
-    for ((j=0; j<sims_per_step; j++))
-    do 
-        echo "Creating map with $i interferentes, simulation $j"
+    echo "Starting simulation $i"
+    int_num=$(generate_triangular)
 
-        # Crear rutas (solor urban)
-        python3 /opt/sumo/tools/randomTrips.py \
-                -v -b 0 -e 500 -p 1 \
-                -n ./sumo/${scenario}/map_${scenario}.net.xml \
-                -r ./sumo/${scenario}/map_${scenario}.rou.xml
-        
-        # Limpiar fichero temporal
-        rm -f trips.trips.xml
+    # Crear rutas (solor urban)
+    python3 /opt/sumo/tools/randomTrips.py \
+            -v -b 0 -e 500 -p 1 \
+            -n ./sumo/${scenario}/map_${scenario}.net.xml \
+            -r ./sumo/${scenario}/map_${scenario}.rou.xml
+    # Limpiar fichero temporal
+    rm -f trips.trips.xml
 
-        # Crear rutas de pelotón e interferentes
-        python3 ./sumo/${scenario}/make_${scenario}_map.py "$i" "$script_path"
+    # Crear rutas de pelotón e interferentes
+    python3 ./sumo/${scenario}/make_${scenario}_map.py "$int_num" "$script_path"
 
-        # Ejecutar SUMO
-        sumo -c ./sumo/${scenario}/map_${scenario}.sumocfg --fcd-output ./sumo/${scenario}/trace_${scenario}.xml
+    # Ejecutar SUMO
+    sumo -c ./sumo/${scenario}/map_${scenario}.sumocfg --fcd-output ./sumo/${scenario}/trace_${scenario}.xml
 
-        # Exportar trace a formato NS2/Mobility
-        python3 /opt/sumo/tools/traceExporter.py \
-            -i ./sumo/${scenario}/trace_${scenario}.xml \
-            --ns2mobility-output="./mapas/$scenario/mobility_int_${i}_sim_${j}.tcl" 
-    done
+    # Exportar trace a formato NS2/Mobility
+    python3 /opt/sumo/tools/traceExporter.py \
+        -i ./sumo/${scenario}/trace_${scenario}.xml \
+        --ns2mobility-output="./mapas/mobility_${i}.tcl" 
+    
+    # Append the generated int_num to the summary file
+    echo "$int_num" >> ./mapas/int_num.txt
 done
